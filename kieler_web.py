92fcdf61500250280b0bc933917f9d0e6f3d8c9a
Derek Lockhart <lockhart@csl.cornell.edu>
2015-04-21 12:04:21 -0400

    more tests and simpler svg req interface

--- kieler_web.py
+++ kieler_web.py
@@ -1,4 +1,6 @@
 import json
+import json_to_svg
+import pytest
 import re
 import requests
 
@@ -7,35 +9,66 @@ UNI_KIEL = 'http://layout.rtsys.informatik.uni-kiel.de:9444/layout'
 #-----------------------------------------------------------------------
 # request_layout
 #-----------------------------------------------------------------------
-def request_layout( kieler_json_obj, kieler_server_url=UNI_KIEL ):
+def request_layout( kieler_json_req, kieler_server_url=UNI_KIEL ):
+  '''Send a layout request to the specified kieler layout server.
 
-  # we need to convert nested json into string format
-  r = requests.post( kieler_server_url, data = kieler_json_obj )
-  assert r.ok
+     Expects input request object to be a dictionary containing the
+     following fields: config, iFormat, oFormat, graph.
+  '''
+
+  # encode json elements (config and graph) as json strings
+  for key, value in kieler_json_req.items():
+    if not isinstance( value, (str,int) ):
+      kieler_json_req[key] = json.dumps( value )
+
+  # post the request
+  r = requests.post( kieler_server_url, data = kieler_json_req )
+
   print r.headers
+  assert r.ok
   return r.text
 
 #-----------------------------------------------------------------------
-# fixup_svg
+# fixup_kwebs_svg
 #-----------------------------------------------------------------------
-def fixup_svg( svg_data ):
+def fixup_kwebs_svg( svg_data ):
+  '''Fixup the broken SVG returned by University of Kiel test server.
+
+   Test server returns illegal SVG with multiple "w" attributes defined,
+   this utility function fixes that up.
+  '''
   return re.sub(' w="[0-9]+" ',' ', svg_data, count=1)
 
 #-----------------------------------------------------------------------
-# request_svg
+# json_graph_to_svg
 #-----------------------------------------------------------------------
-#def request_svg( kieler_json_obj, kieler_server_url=UNI_KIEL ):
-#
-#  if kieler_server_url == UNI_KIEL:
-#    'oFormat' : 'org.w3.svg',
-#    svg_data = request_layout( kieler_json_obj, kieler_server_url )
-#    svg_data = fixup_svg( svg_data )
-#
-#  else:
-#    #'oFormat' : 'org.json',
-#    json_data = request_layout( kieler_json_obj, kieler_server_url )
-#    svg_data  = json_to_svg( json_data )
+def json_graph_to_svg( object_graph, kieler_server_url=UNI_KIEL ):
+  '''Take an object graph and request an svg image with diagram layout.
 
+   Object graph is a hierarchical datastructure of dicts and lists
+   (the Python object equivalent of JSON).
+  '''
 
+  json_req = {
+    'config'  : {
+                'spacing'     : 100,
+                'algorithm'   : 'de.cau.cs.kieler.klay.layered',
+                'edgeRouting' : 'ORTHOGONAL'
+                },
+    'iFormat' : 'org.json',
+    'oFormat' : None,
+    'graph'   : object_graph,
+  }
 
+  if kieler_server_url == UNI_KIEL:
+    json_req['oFormat'] = 'org.w3.svg'
+    svg_data = request_layout( json_req, kieler_server_url )
+    svg_data = fixup_kwebs_svg( svg_data )
 
+  else:
+    json_req['oFormat'] = 'org.json'
+    json_data = request_layout( json_req, kieler_server_url )
+    json_obj  = json.loads( json_data )
+    svg_data  = json_to_svg.json_to_svg( json_obj )
+
+  return svg_data
--- kieler_web_test.py
+++ kieler_web_test.py
@@ -1,21 +1,26 @@
 import os
-import pytest
 import json
+import pytest
+import requests
+
 import json_to_svg as j2s
 import kieler_web  as kw
 
+#-----------------------------------------------------------------------
+# test_request_layout
+#-----------------------------------------------------------------------
 @pytest.mark.parametrize('oFormat', ['org.w3.svg', 'org.json'])
 def test_request_layout( oFormat ):
 
   test_data = {
-    'config'  : json.dumps({
+    'config'  : {
                 'spacing'     : 100,
                 'algorithm'   : 'de.cau.cs.kieler.klay.layered',
                 'edgeRouting' : 'ORTHOGONAL'
-                }),
+                },
     'iFormat' : 'org.json',
     'oFormat' : oFormat,
-    'graph'   : json.dumps({
+    'graph'   : {
                 'id': "root",
                 'children': [
                   {'id': "n1", 'labels': [{'text':"n1"}], 'width':100, 'height':100},
@@ -35,16 +40,22 @@ def test_request_layout( oFormat ):
                   {'id':"e2", 'labels': [{'text':"e2"} ],
                    'source':"n1", 'target': "n5" }
                 ]
-                }),
+                },
   }
 
+  # request the layout
   output = kw.request_layout( test_data )
 
+  # request the layout
   if   'svg'  in oFormat:
-    svg_text = kw.fixup_svg( output )
+    svg_text = kw.fixup_kwebs_svg( output )
   elif 'json' in oFormat:
     json_text = json.loads( output )
     svg_text  = j2s.json_to_svg( json_text )
+  else:
+    assert "Unsupported request type!" == True
+
+  # save the output to a file
 
   filen  = oFormat+'.svg'
   if os.path.exists( filen ):
@@ -55,3 +66,47 @@ def test_request_layout( oFormat ):
 
   assert os.path.exists( filen )
 
+#-----------------------------------------------------------------------
+# test_json_graph_to_svg
+#-----------------------------------------------------------------------
+@pytest.mark.parametrize('server', [
+  'http://layout.rtsys.informatik.uni-kiel.de:9444/layout',
+  'http://localhost:9444/live',
+])
+def test_json_graph_to_svg( server ):
+  graph = { 'id': "root",
+          'children': [
+            {'id': "n1", 'labels': [{'text':"n1"}], 'width':100, 'height':100},
+            {'id': "n2", 'labels': [{'text':"n2"}], 'width':100, 'height': 50,
+             'ports': [{ 'id': "n2_p1", 'width': 10, 'height': 10 }],
+             'children': [
+               {'id': "n3", 'labels': [{'text':"n3" }], 'width':20, 'height':20 },
+               {'id': "n4", 'labels': [{'text':"n4" }], 'width':20, 'height':20 }
+             ],
+             'edges': [{'id': "e4", 'source': "n3", 'target': "n4"}]
+            },
+            {'id': "n5", 'labels': [ {'text':"n5"} ], 'width':100, 'height':50}
+          ],
+          'edges': [
+            {'id':"e1", 'labels': [{'text':"e1"} ],
+             'source':"n1", 'target': "n2", 'targetPort': "n2_p1"},
+            {'id':"e2", 'labels': [{'text':"e2"} ],
+             'source':"n1", 'target': "n5" }
+          ]
+          }
+
+  try:
+    svg_text = kw.json_graph_to_svg( graph, server )
+  except requests.ConnectionError as e:
+    if 'Connection refused' in str( e.message ):
+      pytest.skip('Cannot connect to server.')
+    raise e
+
+  filen  = 'kwebs.svg' if 'kiel' in server else 'localhost.svg'
+  if os.path.exists( filen ):
+    os.remove( filen )
+
+  with open( filen, 'w' ) as fp:
+    fp.write( svg_text )
+
+  assert os.path.exists( filen )
